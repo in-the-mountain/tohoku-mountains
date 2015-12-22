f6d3a5a5e663aae6828e2bdbf5dc3b5caf7590ef
in_the_mountain <uchiyama4510@gmail.com>
2015-12-22 22:52:18 +0000

    page

--- js/run.js
+++ js/run.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
+'jQuery' in window && (function($){
+    'use strict';
     
+    //current
+    $(function () {
+        var url = window.location.href;
+        var filename = url.match(".+/(.+?)\.[a-z]+([\?#;].*)?$")[1];
+        $('#results').text(filename);
+
+    });
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-});
+
+
+
+
+
+
+})(jQuery);
