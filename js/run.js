0e2b5c0171aa60d80af455202eb41156fc020718
pj-yama <nnn06uchi@gmail.com>
2015-12-23 22:03:27 +0900

    いったん完了

--- js/run.js
+++ js/run.js
@@ -2,23 +2,33 @@
     'use strict';
     
     //current
+    //表示中のurlを取得して指定したセレクタの中に同じurlのリンクがあったときにクラスを付与するというもの
     $(function(){
         var url = window.location.pathname;
         $('nav li a[href="'+url+'"]').parent().addClass('current');
     });
     
     //scroll
+    //#で始まるリンクをクリックするとアンカーを設定しているid要素までの位置を取得してアニメーションするというもの
     $(function(){
         $('a[href^=#]').click(function() {
             var speed = 300;
             var href= $(this).attr("href");
-            var target = $(href == "#" || href == "" ? 'html' : href);
+            //#だけのリンクの時はhtmlタグをセレクタとする
+            var target = $(href === "#" || href === "" ? 'html' : href);
             var position = target.offset().top;
             $('body,html').animate({scrollTop:position}, speed, 'swing');
             return false;
         });
     });
-
+    //roll-over
+    $(function(){
+        $('a img').hover(function(){
+            $(this).attr('src', $(this).attr('src').replace('_o', '_h'));
+        }, function(){
+                $(this).attr('src', $(this).attr('src').replace('_h', '_o'));
+        });
+    });
 
 
 
