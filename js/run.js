06bfaa7cc847ae13e83a46e6f184a83e41d42cec
pj-yama <nnn06uchi@gmail.com>
2015-12-23 11:37:19 +0900

    スムーズスクロール追加

--- js/run.js
+++ js/run.js
@@ -6,6 +6,19 @@
         var url = window.location.pathname;
         $('nav li a[href="'+url+'"]').parent().addClass('current');
     });
+    
+    //scroll
+    $(function(){
+        $('a[href^=#]').click(function() {
+            var speed = 300;
+            var href= $(this).attr("href");
+            var target = $(href == "#" || href == "" ? 'html' : href);
+            var position = target.offset().top;
+            $('body,html').animate({scrollTop:position}, speed, 'swing');
+            return false;
+        });
+    });
+
 
 
 
